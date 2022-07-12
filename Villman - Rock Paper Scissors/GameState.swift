70d9e37e9a6cf94aca51705942611ce5cddedb46
Korrin Villman <mrsvillman@gmail.com>
2022-07-11 20:23:30 -0400

    p375

---
+++ Villman - Rock Paper Scissors/GameState.swift
@@ -0,0 +1,13 @@
+//
+//  GameState.swift
+//  Villman - Rock Paper Scissors
+//
+//  Created by Villman, Korrin E on 7/11/22.
+//
+
+import Foundation
+
+enum GameState {
+    case start, win, lose, draw
+}
+
